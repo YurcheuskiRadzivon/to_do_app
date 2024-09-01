db18039031aaa98e5e9d43d7610fb0be55a5cc40
Yurcheuski Radzivon <yurcheuskiradzivon@gmail.com>
2024-09-01 20:00:07 +0300

    Graceful Shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
-	"github.com/YurcheuskiRadzivon/to_do_app"
+	//"fmt"
+
+	"fmt"
 	"log"
+	"net/http"
+	"time"
+
+	"github.com/YurcheuskiRadzivon/to_do_app"
+	"github.com/gorilla/mux"
 )
 
+func homePage(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("hello world mzf"))
+}
 func main() {
+	var wait time.Duration = 15 * time.Second
+	//flag.DurationVar(&wait, "timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	//flag.Parse()
+	fmt.Println("Graceful timeout:", wait)
 	srv := new(to_do_app.Server)
-	err := srv.Run("8080")
-	if err != nil {
-		log.Fatalf("error occured while running http server", err.Error())
-	}
+	r := mux.NewRouter()
+	r.HandleFunc("/", homePage)
+	go func() {
+
+		if err := srv.Run("8080", r); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("error occured while running http server", err.Error())
+		}
+	}()
+	srv.SignalWaiting(wait)
 
 }
