889fe125940cdf7f663bee17282b8215e34c74d3
Yurcheuski Radzivon <yurcheuskiradzivon@gmail.com>
2024-10-01 02:13:01 +0300

    fixed support static files

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -6,7 +6,7 @@ import (
 )
 
 func MainPage(w http.ResponseWriter, r *http.Request) {
-	tmplPath := filepath.Join("..", "templates", "main_page.html")
+	tmplPath := filepath.Join("..", "templates", "home.html")
 	tmpl := ParseFiles(w, tmplPath)
 	err := tmpl.Execute(w, nil)
 	if err != nil {
